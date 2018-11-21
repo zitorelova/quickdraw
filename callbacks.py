ffef942853ae8214778e30b4383d2e42c52d6c3b
DeepLearning VM <google-dl-platform@googlegroups.com>
2018-11-21 13:31:04 +0000

    Add custom callback

---
+++ callbacks.py
@@ -0,0 +1,20 @@
+from fastai import *
+
+class SaveBestModel(LossRecorder):
+    def __init__(self, model, lr, name='best_model'):
+        super().__init__(model.get_layer_opt(lr, None))
+        self.name = name
+        self.model = model
+        self.best_loss = None
+        self.best_acc = None
+
+    def on_epoch_end(self, metrics):
+        super().on_epoch_end(metrics)
+        loss, acc = metrics
+        if self.best_acc == None or acc > self.best_acc:
+            self.best_acc = acc
+            self.best_loss = loss
+            self.model.save(f'{self.name}')
+        elif acc == self.best_acc and  loss < self.best_loss:
+            self.best_loss = loss
+            self.model.save(f'{self.name}')
