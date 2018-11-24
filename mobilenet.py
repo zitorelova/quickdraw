2103bfc529b0819148f1c29cbafc75b8e930f6dd
DeepLearning VM <google-dl-platform@googlegroups.com>
2018-11-24 02:30:57 +0000

    Add tensorflow mobilenet model

---
+++ mobilenet.py
@@ -0,0 +1,19 @@
+import os
+import ast
+import datetime as dt
+import cv2
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
+from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy, categorical_crossentropy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+start = dt.datetime.now()
+
