dd7ee57b444658da5c5e73a1b8ee981cf9481263
Zito Relova <zitorelova@gmail.com>
2018-11-25 16:12:37 +0800

    Add scripts for tensorflow training

--- mobilenet.py
+++ mobilenet.py
@@ -14,6 +14,43 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCh
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications import MobileNet
 from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow_functions import *
 
 start = dt.datetime.now()
 
+BASE_SIZE = 256
+NCSVS = 100
+NCATS = 340
+np.random.seed(seed=1987)
+tf.set_random_seed(seed=1987)
+
+TEPS = 1000
+EPOCHS = 10
+size = 128
+batchsize = 1400
+
+model = MobileNet(input_shape=(size, size, 1), alpha=1., weights=None, classes=NCATS)
+model.load_weights('')
+model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy',
+                      metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
+
+valid_df = pd.read_csv(os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(NCSVS - 1)), nrows=35000)
+x_valid = df_to_image_array_xd(valid_df, size)
+y_valid = keras.utils.to_categorical(valid_df.y, num_classes=NCATS)
+print(x_valid.shape, y_valid.shape)
+print('Validation array memory {:.2f} GB'.format(x_valid.nbytes / 1024.**3 ))
+
+train_datagen = image_generator_xd(size=size, batchsize=batchsize, ks=range(NCSVS - 1))
+
+callbacks = [ReduceLROnPlateau(monitor='val_categorical_accuracy', factor=0.5, patience=5,
+		   min_delta=0.0005, mode='max', cooldown=3, verbose=1), 
+		ModelCheckpoint('models/mobilenet-best-run.h5', monitor='val_categorical_accuracy',
+		mode='max', save_best_only=True, verbose=1)]
+
+hists = []
+hist = model.fit_generator(
+    train_datagen, steps_per_epoch=STEPS, epochs=EPOCHS, verbose=1,
+    validation_data=(x_valid, y_valid),
+    callbacks = callbacks
+)
+hists.append(hist)
---
+++ tensorflow_functions.py
@@ -0,0 +1,74 @@
+def f2cat(filename: str) -> str:
+    return filename.split('.')[0]
+
+def list_all_categories():
+    files = os.listdir(os.path.join(INPUT_DIR, 'train_simplified'))
+    return sorted([f2cat(f) for f in files], key=str.lower)
+
+def apk(actual, predicted, k=3):
+    """
+    Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=3):
+    """
+    Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+def preds2catids(predictions):
+    return pd.DataFrame(np.argsort(-predictions, axis=1)[:, :3], columns=['a', 'b', 'c'])
+
+def top_3_accuracy(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
+
+def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
+    img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
+    for t, stroke in enumerate(raw_strokes):
+        for i in range(len(stroke[0]) - 1):
+            color = 255 - min(t, 10) * 13 if time_color else 255
+            _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
+    if size != BASE_SIZE:
+        return cv2.resize(img, (size, size))
+    else:
+        return img
+
+def image_generator_xd(size, batchsize, ks, lw=6, time_color=True):
+    while True:
+        for k in np.random.permutation(ks):
+            filename = os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(k))
+            for df in pd.read_csv(filename, chunksize=batchsize):
+                df['drawing'] = df['drawing'].apply(ast.literal_eval)
+                x = np.zeros((len(df), size, size, 1))
+                for i, raw_strokes in enumerate(df.drawing.values):
+                    x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw,
+                                             time_color=time_color)
+                x = preprocess_input(x).astype(np.float32)
+                y = keras.utils.to_categorical(df.y, num_classes=NCATS)
+                yield x, y
+
+def df_to_image_array_xd(df, size, lw=6, time_color=True):
+    df['drawing'] = df['drawing'].apply(ast.literal_eval)
+    x = np.zeros((len(df), size, size, 1))
+    for i, raw_strokes in enumerate(df.drawing.values):
+        x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+    x = preprocess_input(x).astype(np.float32)
+    return x
+
+
