070160c50fc7077a633a08c7f2babd8b24eb11b6
zito_relova <zitorelova@gmail.com>
2018-11-29 21:26:48 +0000

    Fix the one cycle policy

--- clr.py
+++ clr.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import warnings
 
-from keras.callbacks import Callback
-from keras import backend as K
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras import backend as K
 
 
 # Code is ported from https://github.com/fastai/fastai
